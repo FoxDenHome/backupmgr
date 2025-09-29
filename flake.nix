0aabde29ca9fd9b7dc907f7201d032daea64573a
Doridian <git@doridian.net>
2025-09-29 12:58:11 -0700

    Nixify

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "backupmgr";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = null;
+            buildInputs = [];
+          };
+        };
+      });
+}
